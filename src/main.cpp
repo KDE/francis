accef89a09b1e6e49be2ba699b8fe60cacf5bc97
Felipe Kinoshita <kinofhek@gmail.com>
2022-06-21 06:01:00 -0300

    Remove QCoreApplication::setApplicationName
    KAboutData::setApplicationData already does this for us.

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,6 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
-    QCoreApplication::setApplicationName(QStringLiteral("francis"));
 
     KAboutData aboutData(
                          // The program name used internally.
