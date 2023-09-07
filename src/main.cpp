dff5659740b91cc2a8cb926b7507f79368e46e87
Carl Schwan <carl@carlschwan.eu>
2023-09-07 23:12:32 +0200

    Include QtGlobal

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Felipe Kinoshita <kinofhek@gmail.com>
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+#include <QtGlobal>
 #ifdef Q_OS_ANDROID
 #include <QGuiApplication>
 #else
